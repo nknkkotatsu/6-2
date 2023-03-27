32df0b1d6f2849d0f1c48b7d768bc332b0228184
EC2 Default User <ec2-user@ip-172-31-21-137.ap-northeast-1.compute.internal>
2023-03-27 13:41:29 +0000

    [Fnish]6-3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ before_action :is_matching_login_user, only: [:edit]
     @books = Book.new
     @book = Book.find(params[:id])
     @user = @book.user
+    @book_comment = BookComment.new
   end
 
   def index
